bf715cefc31ca9637df54952ff3678204b4c68ec
Yan Shvyndikov <Yansv2006@gmail.com>
2023-04-24 13:55:37 +0300

    Creation
    Creation and distribution by files with

---
+++ MobileUpGallery/UGLoginViewController.swift
@@ -0,0 +1,14 @@
+
+import UIKit
+
+final class UGLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
